c6ebd9976539bd3140f5400a88c8a2cb74fd8386
SychO <vbdd@msn.com>
2025-09-02 15:14:17 +0800

    Improve docstring formatting and clarity in answer_callback_query.py

--- pyrogram/methods/bots/answer_callback_query.py
+++ pyrogram/methods/bots/answer_callback_query.py
@@ -29,7 +29,7 @@ class AnswerCallbackQuery:
         url: str = None,
         cache_time: int = 0
     ):
-        """Send answers to callback queries sent from inline keyboards.
+        """Use this method to send answers to callback queries sent from inline keyboards.
         The answer will be displayed to the user as a notification at the top of the chat screen or as an alert.
 
         .. include:: /_includes/usable-by/bots.rst
@@ -42,14 +42,13 @@ class AnswerCallbackQuery:
                 Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters.
 
             show_alert (``bool``, *optional*):
-                If true, an alert will be shown by the client instead of a notification at the top of the chat screen.
+                If True, an alert will be shown by the client instead of a notification at the top of the chat screen.
                 Defaults to False.
 
             url (``str``, *optional*):
                 URL that will be opened by the user's client.
-                If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your
-                game – note that this will only work if the query comes from a callback_game button.
-                Otherwise, you may use links like t.me/your_bot?start=XXXX that open your bot with a parameter.
+                If you have created a Game and accepted the conditions via @BotFather,
+                specify the URL that opens your game - note that this will only work if the query comes from a callback_game button.
 
             cache_time (``int``, *optional*):
                 The maximum amount of time in seconds that the result of the callback query may be cached client-side.
